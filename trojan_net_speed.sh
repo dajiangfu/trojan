251917265aa51452de544e33611c768ed32e113a
dajiangfu <60643767+dajiangfu@users.noreply.github.com>
2020-03-08 23:18:14 +0800

    Add files via upload

--- trojan_net_speed.sh
+++ trojan_net_speed.sh
@@ -63,9 +63,9 @@ EOF
 #改变SSH端口号
 function change_ssh_port(){
   read -p "请输入新端口号:" port_num
-  sed '/#Port 22/a\Port $port_num' /etc/ssh/sshd_config
+  sed '/#Port 22/a\Port ${port_num}' /etc/ssh/sshd_config
   sed -i 's/#Port 22/Port 22/g' /etc/ssh/sshd_config
-  firewall-cmd --zone=public --add-port=$port_num/tcp --permanent
+  firewall-cmd --zone=public --add-port=${port_num}/tcp --permanent
   firewall-cmd --reload
   systemctl restart sshd.service
 }
