2969a681b8fdab708b8fd8bd9e94c1b254bee5fd
dajiangfu <60643767+dajiangfu@users.noreply.github.com>
2020-09-03 22:30:38 +0800

    Add files via upload

--- trojan_mult.sh
+++ trojan_mult.sh
@@ -297,12 +297,12 @@ function install_trojan(){
       if [[ $yn == [Yy] ]]; then
         green "添加放行80/443端口规则"
         $systemPackage -y install policycoreutils-python
-        if [ semanage port -l|grep  http_port_t|grep -w 80 ]; then
+        if semanage port -l|grep  http_port_t|grep -w 80 >/dev/null 2>/dev/null; then
           green "80端口已添加"
         else
           semanage port -a -t http_port_t -p tcp 80
         fi
-        if [ semanage port -l|grep  http_port_t|grep -w 443 ]; then
+        if semanage port -l|grep  http_port_t|grep -w 443 >/dev/null 2>/dev/null; then
           green "443端口已添加"
         else
           semanage port -a -t http_port_t -p tcp 443
